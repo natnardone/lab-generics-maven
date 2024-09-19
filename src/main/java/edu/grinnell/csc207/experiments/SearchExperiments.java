7774c972f9b7c11a34fd6cc73f460633acdc3616
Natalie Nardone <nardonen@grinnell.edu>
2024-09-19 09:19:22 -0500

    Complete work from exercise 2.

--- src/main/java/edu/grinnell/csc207/experiments/SearchExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/SearchExperiments.java
@@ -3,6 +3,7 @@ package edu.grinnell.csc207.experiments;
 import edu.grinnell.csc207.util.SearchUtils;
 import edu.grinnell.csc207.util.predicates.OddInteger;
 import edu.grinnell.csc207.util.predicates.SmallString;
+import edu.grinnell.csc207.util.predicates.SmallObject;
 
 import java.io.PrintWriter;
 
@@ -24,8 +25,8 @@ public class SearchExperiments {
     PrintWriter pen = new PrintWriter(System.out, true);
 
     // Set up a few arrays to search.
-    String[] strings =
-        new String[] {"alpha", "bravo", "charlie", "delta", "echo", "foxtrot",
+    Object[] strings =
+        new Object[] {"alpha", "bravo", "charlie", "delta", "echo", "foxtrot",
             "golf", "hotel", "india", "juliett", "kilo", "lima", "mike",
             "november", "oscar"};
     Integer[] numbers =
@@ -33,7 +34,7 @@ public class SearchExperiments {
 
     // Okay, we're ready for the experiments
     pen.println("A small string: "
-        + SearchUtils.search(strings, SmallString.PRED));
+        + SearchUtils.search(strings, SmallObject.PRED));
     pen.println("An odd integer: "
         + SearchUtils.search(numbers, OddInteger.PRED));
   } // main(String[])
